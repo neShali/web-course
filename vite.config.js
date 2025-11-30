6544d65674fd6c60bfe10038dbf113322d02ba9e
Shmelev Anton <poshtet.spb@gmail.com>
2025-11-30 23:49:44 +0300

    deploy to githubpages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // !!! ВАЖНО для GitHub Pages
-  // 1) Если репозиторий username.github.io → оставь '/'.
-  // 2) Если репозиторий, например, 'vasya-portfolio',
-  //    для GitHub Pages по адресу https://username.github.io/vasya-portfolio/
-  //    поставь base: '/vasya-portfolio/'.
-  base: '/',
+  base: '/web-course/',
+
+  build: {
+    outDir: 'docs',
+  },
 });
